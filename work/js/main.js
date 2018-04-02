539165e7dba2ce56482fc7b008adcd064eafb990
Enclave88 <sutangu@gmail.com>
2018-04-02 15:12:51 +0600

    came on

--- work/js/main.js
+++ work/js/main.js
@@ -1,2 +1,19 @@
 'use strict';
 
+var constraints = {
+    video: true
+}
+
+var video = document.querySelector('video');
+
+function handleSuccess(stream) {
+    video.srcObject = stream;
+}
+
+function handleError(error) {
+    console.error('getUserMedia error: ', error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(handleSuccess)
+    .catch(handleError)
